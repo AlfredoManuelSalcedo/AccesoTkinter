1d0698b921cfef98713d256ad58deff870d7247d
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-11 13:47:30 +0100

    Añadido posicionamineto

--- ejer01.py
+++ ejer01.py
@@ -6,6 +6,7 @@ class Application(ttk.Frame):
         super().__init__(main_window)
         main_window.title("Posicionar elementos Tcl/Tk")
         main_window.configure(width=300,height=200)
+        self.place(relwidth=1,relheight=1)
 
 #main
 principal= tk.Tk()
