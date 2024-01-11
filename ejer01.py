c4dbc654e32894b06c368ff56d4c630b6889ee9e
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-11 13:50:40 +0100

    Creación de un boton

--- ejer01.py
+++ ejer01.py
@@ -6,7 +6,11 @@ class Application(ttk.Frame):
         super().__init__(main_window)
         main_window.title("Posicionar elementos Tcl/Tk")
         main_window.configure(width=300,height=200)
+
+        self.button = ttk.Button(self, text="Hola, mundo!")
+        self.button.place(x=60,y=40, width=100,height=30)
         self.place(relwidth=1,relheight=1)
+        
 
 #main
 principal= tk.Tk()
