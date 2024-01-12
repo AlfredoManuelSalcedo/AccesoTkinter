b4e138bf7897bdbcb4e6766e42db0a8f45a2c7b7
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-12 12:57:53 +0100

    linea extra eliminada en ejer02

--- ejer02.py
+++ ejer02.py
@@ -6,7 +6,6 @@ class Application(ttk.Frame):
  def __init__(self, main_window):
     super().__init__(main_window)
     main_window.title("Posicionar elementos en Tcl/Tk")
-    main_window.configure(width=300,height=200)
 
     self.entry = ttk.Entry(self)
     self.entry.pack()
