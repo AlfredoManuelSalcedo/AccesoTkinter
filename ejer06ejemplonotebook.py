da771d128ed4e0755facd018e679839f9f19e711
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-17 12:47:57 +0100

    Eliminado el segundo frame

--- ejer06ejemplonotebook.py
+++ ejer06ejemplonotebook.py
@@ -16,13 +16,6 @@ class Aplicacion:
         self.boton2=ttk.Button(self.pagina1,text="Ejemplo de boton inactivo",state="disabled")
         self.boton2.grid(column=0, row=2)
 
-        self.pagina2 = ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina2, text="Label")
-        self.label2=ttk.Label(self.pagina2, text="La clase Label permite mostrar un mensaje en la ventana")
-        self.label2.grid(column=0,row=0)
-        self.label3=ttk.Label(self.label2,text="con los caracteres \\n podemos hacer un salto de linea")
-        self.label3.grid(column=0,row=1)
-
         self.cuaderno1.grid(column=0,row=0)
         self.ventana1.mainloop()
 
