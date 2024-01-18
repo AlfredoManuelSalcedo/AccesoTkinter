82a18448f230a23fd9e0707592023bb46994a535
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-18 09:54:19 +0100

    creadas funciones claves del ejercicio 7 y movido el ejercicio a una carpeta

---
+++ EjercicioSociosBaloncesto/socio.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+class socioBaloncesto:
+    def __init__(self):
+        self.conexion=mysql.connector.connect(host="localhost", user="root", passwd="root", database="baloncesto")
+    
+    def nuevo_socio(self, datos):
+        cursor=self.conexion.cursor()
+        sql="insert into socio(socioID, nombre, estatura, edad, localidad) values (%s,%s,%s,%s,%s)"
+        cursor.execute(sql, datos)
+        self.conexion.commit()
+    
+    def consultaSocio(self, datos):
+        cursor=self.conexion.cursor()
+        sql="SELECT nombre,estatura,edad,localidad FROM socio where socioID=%s"
+        cursor.execute(sql,datos)
+        vuelta=cursor.fetchall()
+        return vuelta
+    
+    def socio(self):
+        cursor=self.conexion.cursor()
+        sql="SELECT socioID,nombre,estatura,edad,localidad from socio"
+        cursor.execute(sql)
+        vuelta=cursor.fetchall()
+        return vuelta
+    
+    def borrar_socio(self, datos):
+        cursor=self.conexion.cursor()
+        sql="DELETE FROM socio WHERE socioID=%s"
+        cursor.execute(sql,datos)
+        n=cursor.rowcount
+        self.conexion.commit()
+        return n #devolver las filas borradas
+    
+    def modificar(self, datos):
+        cursor=self.conexion.cursor()
+        sql="UPDATE socio set nombre=%s, estatura=%s, edad=%s, localidad=%s where socioID=%s"
+        cursor.execute(sql,datos)
+        n=cursor.rowcount
+        self.conexion.commit()
+        return n #devuelve las filas modificadas
