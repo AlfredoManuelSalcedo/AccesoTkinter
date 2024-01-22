47320c725aba84bda2dada443dd9d9eda40cda57
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-22 10:19:32 +0100

    errores de codigo, relacionados con el estilo solucionados

--- ejer09EjercicioTemperaturas2/conversionTemperaturas.py
+++ ejer09EjercicioTemperaturas2/conversionTemperaturas.py
@@ -5,31 +5,32 @@ import convertir as cv
 
 class Aplicacion:
     def __init__(self,ventana1):
-        s =ttk.Style()
+        s = ttk.Style()
+        s.theme_use("default")
         s.configure("TLabelframe", background="grey")
-        s.configure("TLabelframe.Label",background="grey", foreground="white", font=("verdana",20,"bold"))
-        s.configure("TButton", background="grey",foreground="white",font=("verdana",15,"bold"))
-
-        caracteristicas = {"justify":"left", "background":"FFFFFF","foreground":"black","font":("Verdana",10,"bold"),"width":20}
+        s.configure("TLabelframe.Label", background="grey", foreground="white", font=("verdana", 20, "bold"))
+        s.configure("TButton", background="grey", foreground="white", font=("verdana", 15, "bold"))
+    
+        caracteristicas = {"justify":"left", "background":"#FFFFFF", "foreground":"black", "font":("Verdana", 10, "bold"), "width":20}
         caracteristicas2 = {"justify":"right", "background":"#FFFFFF", "foreground":"black", "font":("Verdana", 10, "bold"), "width":10}
         caracteristicas3 = {"width":3}
 
         self.lf1=ttk.LabelFrame(ventana1, text="Conversiones", labelanchor="nw")
-        self.lf1.grid(column=0,row=0,padx=125,pady=10)
+        self.lf1.grid(column=0,row=0,padx=10,pady=50)
 
         
         ttk.Label(self.lf1, text="Celsius:",**caracteristicas).grid(column=0,row=0,padx=20,pady=20)
         ttk.Label(self.lf1, text="Kelvin:",**caracteristicas).grid(column=0,row=1,pady=20)
         ttk.Label(self.lf1, text="Farhenheit:",**caracteristicas).grid(column=0,row=2,pady=20)
 
-        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gc",v2=1),**caracteristicas).grid(column=2,row=0, padx=10)
-        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gc",v2=-1),**caracteristicas).grid(column=3,row=0)
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gc",v2=1),**caracteristicas3).grid(column=2,row=0, padx=10)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gc",v2=-1),**caracteristicas3).grid(column=3,row=0)
 
-        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gf",v2=1),**caracteristicas).grid(column=2,row=1)
-        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gf",v2=-1),**caracteristicas).grid(column=3,row=1)
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gf",v2=1),**caracteristicas3).grid(column=2,row=1)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gf",v2=-1),**caracteristicas3).grid(column=3,row=1)
 
-        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gk",v2=1),**caracteristicas).grid(column=2,row=2)
-        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gk",v2=-1),**caracteristicas).grid(column=3,row=2)
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gk",v2=1),**caracteristicas3).grid(column=2,row=2)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gk",v2=-1),**caracteristicas3).grid(column=3,row=2)
 
         self.gc = tk.DoubleVar()
         caja1=ttk.Entry(self.lf1,textvariable=self.gc,**caracteristicas2)
