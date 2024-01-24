af57badb6eb06920be01474d4101bb78c3ab664a
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-24 10:06:15 +0100

    pequeño ejercico de repaso de arrays bidimensionales

---
+++ ejer13matrizbidimensionalprueba.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+   matriz = [[1, 2, 3], 
+        [2, 5, 6],
+        [33,44]]
+   for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j], end=' ')
+    print() 
