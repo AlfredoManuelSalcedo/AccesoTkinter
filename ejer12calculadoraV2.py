45aec8434006477519a693f516ce336e7f8db5b4
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-24 09:47:04 +0100

    Calculadora funcional

--- ejer12calculadoraV2.py
+++ ejer12calculadoraV2.py
@@ -6,10 +6,11 @@ class Calculadora(Frame):
         super().__init__(ventana)
         self.grid()
         self.botones()
+        ap.mainloop()
     
     def botones(self):
-        estilo = {"font":("Arial",16),"bg":"white","fg":'red',"highligthbackground":'red'}
-        estilo1 = {"font":("Arial",14),"bg":"white","fg":'black',"highligthbackground":'white'}
+        estilo = {"font":("Arial",16),"bg":"white","fg":'red',"highlightbackground":'red'}
+        estilo1 = {"font":("Arial",14),"bg":"white","fg":'black',"highlightbackground":'white'}
 
         self.display=Entry(self, font=("Arial",24),relief=RIDGE,justify=RIGHT,bg='darkblue',fg='red',borderwidth=0)
         self.display.insert(0,"0")
@@ -36,6 +37,59 @@ class Calculadora(Frame):
         Button(self, text=".",**estilo, command=lambda: self.añadir(".")).grid(row=5,column=2,sticky="nsew")
         Button(self, text="=",**estilo, command=lambda: self.evaluar()).grid(row=5,column=3,sticky="nsew")
 
+    def remplazarTexto(self, text):
+        self.display.delete(0, END)
+        self.display.insert(0, text)
+
+
+    def añadir(self,text):
+        actualText = self.display.get()
+        textLength = len(actualText)
+        if actualText == "0":
+            self.remplazarTexto(text)
+        else:
+            self.display.insert(textLength, text)
+    
+    def evaluar(self):
+        try:
+            self.remplazarTexto(eval(self.display.get()))
+        except (SyntaxError, AttributeError):
+            messagebox.showerror("Error", "SyntaxError")
+            self.remplazarTexto("0")
+        except ZeroDivisionError:
+            messagebox.showerror("Error","No se puede divir entre 0")
+            self.remplazarTexto("0")
+
+    def contineSigno(self):
+        lista= ["*","/","+","-"]
+        display = self.display.get()
+        for c in display:
+            if c in lista:
+                return True
+        return False
+
+    def cambioSigno(self):
+        if self.contineSigno():
+            self.evaluar()
+        firstChar = self.display.get()[0]
+        if firstChar == "0":
+            pass
+        elif firstChar =="-":
+            self.display.delete(0)
+        else:
+            self.display.insert(0,"-")
+    
+    def inversa(self):
+        self.display.insert(0,"1/(")
+        self.añadir(")")
+        self.evaluar()
+
+    def borrarUltimoCaracter(self):
+        textLength= len(self.display.get())
+        if textLength >=1:
+            self.display.delete(textLength-1 , END)
+        elif textLength ==1:
+            self.remplazarTexto("0")
 
 
 
