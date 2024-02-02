f6dbb63d96bcff79765f09bbc15cab14b989730f
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-02-02 12:33:17 +0100

    Update ejer15spinbox.py

--- ejer15spinbox.py
+++ ejer15spinbox.py
@@ -4,18 +4,17 @@ from tkinter import messagebox as mb
 import random
 
 class Aplicacion:
-    def __init__(self):
+    def __init__(self,ventana):
         self.ventana1=tk.Tk()
-        self.label1=ttk.Label(self.ventana1, text="Seleccione la cantidad de bultos:")
+        self.label1=ttk.Label(ventana, text="Seleccione la cantidad de bultos:")
         self.label1.grid(column=0, row=0, padx=10, pady=10)
-        self.spinbox1=ttk.Spinbox(self.ventana1, from_=0, to=100, width=10, state='readonly')        
+        self.spinbox1=ttk.Spinbox(ventana, from_=0, to=100, width=10, state='readonly')        
         self.spinbox1.set(0)        
         self.spinbox1.grid(column=1, row=0, padx=10, pady=10)
-        self.boton1=ttk.Button(self.ventana1, text="Sortear", command=self.sortear)
+        self.boton1=ttk.Button(ventana, text="Sortear", command=self.sortear)
         self.boton1.grid(column=0, row=1, padx=10, pady=10)
-        self.label2=ttk.Label(self.ventana1, text="", width=20)
+        self.label2=ttk.Label(ventana, text="", width=20)
         self.label2.grid(column=1, row=1, padx=10, pady=10)
-        self.ventana1.mainloop()
 
     def sortear(self):
         if int(self.spinbox1.get())==0:
@@ -29,4 +28,17 @@ class Aplicacion:
                 self.label2.configure(text="No se revisan")
                 self.label2.configure(background="green")
 
-aplicacion1=Aplicacion() 
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.configure(padx=30,pady=30)
+        self.title("Calculadora")
+        w=425
+        h=300
+        x = (self.winfo_screenwidth()/2)-(w/2)
+        y = (self.winfo_screenheight()/2)-(h/2)
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.resizable(False,False)
+
+app=App()
+Aplicacion(app)
